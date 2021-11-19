ac4a019d238fd70263c9356726aaa2fb3ef7a90b
Manitajs <manita.paudel@ombryo.com>
2021-11-19 16:03:16 +0545

    Adjust import orders on login page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,8 @@
 import React from "react"
-
-import { Formik, Field, Form } from "formik";
-import * as Yup from "yup";
 import {useHistory} from "react-router-dom"
 import {ToastContainer, toast} from 'react-toastify'
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
 import 'react-toastify/dist/ReactToastify.css';
 
 import axios from "src/utils/axios"
