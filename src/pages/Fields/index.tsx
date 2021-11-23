3c9d2f1cf4c1ff3b7878f2d9f837170bafe4a6e0
Manitajs <manita.paudel@ombryo.com>
2021-11-23 08:15:33 +0545

    Fetch the fields from the API, list them out in the fields page, adjust styles

--- src/pages/Fields/index.tsx
+++ src/pages/Fields/index.tsx
@@ -1,60 +1,79 @@
-import AuthLayout from "src/layout/AuthLayout";
-import { H1, H2 } from "src/components/Typography";
-import { defaultMaxListeners } from "events";
+import React from "react";
 
-const data = [
-  {
-    name: "General",
-    order: 1,
-    field_items: [
-      {
-        title: "General",
-        subtitle: "JPT",
-        order: 1,
-      },
-    ],
-  },
-  {
-    name: "Second One",
-    order: 2,
-    field_items: [
-      {
-        title: "General",
-        subtitle: "JPT",
-        order: 1,
-      },
-    ],
-  },
-  {
-    name: "Third One",
-    order: 3,
-    field_items: [
-      {
-        title: "General",
-        subtitle: "JPT",
-        order: 1,
-      },
-    ],
-  },
-];
+import AuthLayout from "src/layout/AuthLayout";
+import { H2 } from "src/components/Typography";
+import axios from "src/utils/axios";
+
+// const data = [
+//   {
+//     name: "General",
+//     order: 1,
+//     field_items: [
+//       {
+//         title: "General",
+//         subtitle: "JPT",
+//         order: 1,
+//       },
+//     ],
+//   },
+//   {
+//     name: "Second One",
+//     order: 2,
+//     field_items: [
+//       {
+//         title: "General",
+//         subtitle: "JPT",
+//         order: 1,
+//       },
+//     ],
+//   },
+//   {
+//     name: "Third One",
+//     order: 3,
+//     field_items: [
+//       {
+//         title: "General",
+//         subtitle: "JPT",
+//         order: 1,
+//       },
+//     ],
+//   },
+// ];
 
 const Fields = () => {
+
+  const [fields, setFields] = React.useState<any[]>([])
+
+  React.useEffect(() => {
+    const fetchFields = async () => {
+      const {data} = await axios.get("/fields")
+      console.log(data)
+      setFields(data)
+    }
+    fetchFields()
+  }, [])
+  console.log("Fields", fields)
   return (
     <AuthLayout>
       {/* <H1>Fields</H1> */}
       <main className="flex justify-between w-fullrounded-md min-h-screen mt-2">
-        <section className=" bg-gray-50 w-2/3 p-3 mr-2 border border-gray-200 shadow-sm">
-          <H2>Active Fields</H2>
+        <section className=" bg-gray-50 w-2/3 p-3 mr-2 border border-gray-400 shadow-sm">
+          <section className="text-center text-purple mb-2">
+            <H2>Active Fields</H2>
+          </section>
           <ul className="mt-2">
-            {data.map((datum) => (
-              <li key={datum.order} className="my-2">
-                <div className="bg-gray-100 text-lg font-medium p-2 rounded-sm">
-                  {datum.name}
+            {fields?.map((field) => (
+              <li key={field.order} className="my-2">
+                <div className="bg-gray-200 text-lg font-medium p-2 rounded-sm">
+                  {field.name}
                 </div>
-                <ul className="!bg-none py-2">
-                  {datum.field_items.map((item) => (
-                    <li className="text-sm font-normal text-gray-700">
-                      {item.title}({item.subtitle})
+                <ul className="!bg-none p-2">
+                  {field.field_items.map((item) => (
+                    <li
+                      className="text-sm font-normal text-gray-700 p-2"
+                      key={item.title}
+                    >
+                      {item.title}{item.subtitle ? `(${item.subtitle})` : ""}
                     </li>
                   ))}
                 </ul>
@@ -62,8 +81,10 @@ const Fields = () => {
             ))}
           </ul>
         </section>
-        <section className=" bg-gray-50 w-2/3 p-3 border border-gray-200 shadow-sm">
-          <H2>Inactive Fields</H2>
+        <section className=" bg-gray-50 w-2/3 p-3 border border-gray-400 shadow-sm">
+          <section className="text-center text-purple mb-2">
+            <H2>Inactive Fields</H2>
+          </section>
         </section>
       </main>
     </AuthLayout>
