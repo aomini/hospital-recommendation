f39f59f15db3fba0d319008726064fc415ed8fb7
Manitajs <manita.paudel@ombryo.com>
2021-11-23 13:07:03 +0545

    Adjust the 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div className="bg-red-200 h-screen w-full text center">
+    <div className="bg-red-200 h-screen w-full flex items-center justify-center">
       <H1> 404 Page Not Found</H1>
       <Link to="/">Go Home</Link>
     </div>
