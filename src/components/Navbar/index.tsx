c82163227e938f11805836de10b37a369570abc9
Manitajs <manita.paudel@ombryo.com>
2021-11-30 12:53:50 +0545

    Separate the sctive-class checking function to utils folder

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,6 +10,7 @@ import LocationIcon from "src/assets/icons/LocationIcon";
 import SettingsIcon from "src/assets/icons/SettingsIcon";
 import UsersIcon from "src/assets/icons/UsersIcon";
 import { BodyText, Subtitle } from "../Typography";
+import activeLinkChecker from "src/utils/activeLinkChecker";
 
 interface NavbarProps {}
 const navItems = [
@@ -26,6 +27,7 @@ const Navbar: React.FC<NavbarProps> = () => {
   const location = useLocation();
   const history = useHistory();
 
+  const path = location.pathname.split("/");
   const handleOpen = () => {
     setMenuOpen(!menuOpen);
   };
@@ -38,7 +40,8 @@ const Navbar: React.FC<NavbarProps> = () => {
   useClickOutside(menuRef, () => {
     setMenuOpen(false);
   });
-
+  console.log(location);
+  console.log(path);
   return (
     <UserContext.Consumer>
       {(value: any) => (
@@ -48,12 +51,10 @@ const Navbar: React.FC<NavbarProps> = () => {
               {navItems.map(({ name, href, Icon }) => (
                 <Link
                   to={href}
-                  className={`text-base flex flex-col justify-center items-center p-3 hover:bg-purple hover:text-gray-50 font-medium ${
-                    (href !== "/" && location.pathname.includes(href)) ||
-                    location.pathname === href
-                      ? "text-purple border-b-4 border-purple"
-                      : "text-gray-600"
-                  }`}
+                  className={`text-base flex flex-col justify-center items-center p-3 hover:bg-purple hover:text-gray-50 font-medium ${activeLinkChecker(
+                    href,
+                    location.pathname
+                  )}`}
                   key={href}
                 >
                   <Icon className="mx-auto h-8 w-8" />
